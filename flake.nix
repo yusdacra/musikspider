a5bd74cc2aa3564dc2714b538107bbd0fb00ec1b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-04-12 07:21:16 +0300

    chore: add envrc and gitignore, make flake work

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
+  inputs.naked-shell.flake = false;
 
   outputs = inp:
     inp.parts.lib.mkFlake {inputs = inp;} {
@@ -12,7 +13,7 @@
       perSystem = {pkgs, ...}: let
         mkNakedShell = pkgs.callPackage inp.naked-shell {};
       in {
-        devShell = mkNakedShell {
+        devShells.default = mkNakedShell {
           name = "musikspider-devshell";
           packages = with pkgs; [deno];
         };
