7901dd3f1fdd325b14b3ffcb2cc8d8c43b5c8d91
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-04-12 11:09:04 +0300

    chore: update flake

--- flake.nix
+++ flake.nix
@@ -5,15 +5,13 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.systems.url = "github:nix-systems/x86_64-linux";
   inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
-  inputs.naked-shell.flake = false;
 
   outputs = inp:
     inp.parts.lib.mkFlake {inputs = inp;} {
       systems = import inp.systems;
-      perSystem = {pkgs, ...}: let
-        mkNakedShell = pkgs.callPackage inp.naked-shell {};
-      in {
-        devShells.default = mkNakedShell {
+      imports = [inp.naked-shell.flakeModule];
+      perSystem = {config, pkgs, ...}: {
+        devShells.default = config.mk-naked-shell.lib.mkNakedShell {
           name = "musikspider-devshell";
           packages = with pkgs; [
             deno
