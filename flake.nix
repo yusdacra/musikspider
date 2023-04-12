a0d87a05f86e12509ab57b550f12a5d9572f3796
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-04-12 07:16:36 +0300

    chore: init

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.systems.url = "github:nix-systems/x86_64-linux";
+  inputs.naked-shell.url = "github:yusdacra/mk-naked-shell";
+
+  outputs = inp:
+    inp.parts.lib.mkFlake {inputs = inp;} {
+      systems = import inp.systems;
+      perSystem = {pkgs, ...}: let
+        mkNakedShell = pkgs.callPackage inp.naked-shell {};
+      in {
+        devShell = mkNakedShell {
+          name = "musikspider-devshell";
+          packages = with pkgs; [deno];
+        };
+      };
+    };
+}
