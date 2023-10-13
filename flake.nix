89f99b7324c0dcc37e4d7a8eb4ab1f3a75a349cc
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2023-10-13 19:04:32 +0300

    build: fix nix build

--- flake.nix
+++ flake.nix
@@ -17,22 +17,75 @@
         pkgs,
         system,
         ...
-      }: {
+      }: let
+        packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+      in {
         devShells.default = config.mk-naked-shell.lib.mkNakedShell {
           name = "musikspider-devshell";
-          packages = with pkgs; [nodejs_20 bun];
+          packages = with pkgs; [nodejs-slim_latest bun];
           shellHook = ''
             export PATH="$PATH:$PWD/node_modules/.bin"
           '';
         };
-        # packages.musikspider = pkgs.mkYarnPackage {
-        #   src = ./.;
+        packages.musikspider-modules = pkgs.stdenv.mkDerivation {
+          pname = "${packageJson.name}-modules";
+          version = packageJson.version;
 
-        #   buildPhase = "HOME=$TMPDIR yarn --offline build";
-        #   distPhase = "true";
-        #   installPhase = "mv deps/musikspider/build $out";
-        # };
-        # packages.default = config.packages.musikspider;
+          src = ./.;
+          
+          outputHash = "sha256-X3+MtWZV42p1A3TmByJ6i8D2B5iY3rfbEw603Az8AUI=";
+          outputHashAlgo = "sha256";
+          outputHashMode = "recursive";
+
+          nativeBuildInputs = with pkgs; [bun];
+
+          dontConfigure = true;
+          impureEnvVars = pkgs.lib.fetchers.proxyImpureEnvVars
+            ++ [ "GIT_PROXY_COMMAND" "SOCKS_SERVER" ];
+
+          buildPhase = "bun install --no-progress --frozen-lockfile";
+          installPhase = "mv node_modules $out";
+        };
+        packages.musikspider = pkgs.stdenv.mkDerivation {
+          pname = packageJson.name;
+          version = packageJson.version;
+
+          src = ./.;
+
+          nativeBuildInputs = [pkgs.makeBinaryWrapper];
+          buildInputs = [pkgs.bun];
+
+          LOCAL_MUSIKQUAD_SERVER="http://127.0.0.1:5505";
+          PUBLIC_MUSIKQUAD_SERVER="127.0.0.1:5505";
+          PUBLIC_BASEURL="localhost:5173";
+
+          configurePhase = ''
+            runHook preConfigure
+            cp -R ${config.packages.musikspider-modules} node_modules
+            substituteInPlace node_modules/.bin/vite \
+              --replace "/usr/bin/env node" "${pkgs.nodejs-slim_latest}/bin/node"
+            runHook postConfigure
+          '';
+          buildPhase = ''
+            runHook preBuild
+            bun --prefer-offline run build
+            runHook postBuild
+          '';
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/bin
+            ln -s ${config.packages.musikspider-modules} $out
+            cp -R ./build/* $out
+
+            makeBinaryWrapper ${pkgs.bun}/bin/bun $out/bin/${packageJson.name} \
+              --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.bun ]} \
+              --add-flags "run --prefer-offline --no-install --cwd $out start"
+
+            runHook postInstall
+          '';
+        };
+        packages.default = config.packages.musikspider;
       };
     };
 }
